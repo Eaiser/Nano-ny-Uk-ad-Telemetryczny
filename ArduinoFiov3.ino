c570adcb95bea5179f79a1cf691747641f24b043
Michal <48297259+Eaiser@users.noreply.github.com>
2019-12-17 21:56:34 +0100

    Update ArduinoFiov3.ino
    module 30 m/s2

--- ArduinoFiov3.ino
+++ ArduinoFiov3.ino
@@ -10,6 +10,7 @@ byte gatewayMac[8] = {0x00,0x13,0xA2,0x00,0x41,0x7C,0x48,0x1E};
 int frameType = 99;
 bool frame = false;
 bool request =false;
+bool moduleFlag = false;
 File plik;
 
 void setup() {
@@ -78,7 +79,10 @@ void receiveEvent(int howMany) {
   while (1 < Wire.available()) { 
     
    data[i] = Wire.read();
-    if(i>4){
+   if((char)data[0] == '3' && (char)data[2] == '0' && (char)data[3] == '5') {
+    moduleFlag = true; 
+   }
+     if(i>4){
      sendData[x] = data[i];
      x++;
    }
@@ -92,12 +96,11 @@ void receiveEvent(int howMany) {
  if(counter == 4){
   x = 0;
   counter = 0;
-  if(request == true){
+  if(request == true || moduleFlag){
     Serial.println();
     generateFrame(3,gatewayMac,sendData);
+   }
   }
- }
-
 
 }
 
